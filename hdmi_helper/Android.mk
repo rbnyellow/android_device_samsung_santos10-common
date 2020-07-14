d2f47b7e88328c6cdb0d824648d51111082aa16c
rbnyellow <rbnyellow@gmail.com>
2020-07-14 19:11:38 +0200

    santos10 : update hdmi_helper lib

--- hdmi_helper/Android.mk
+++ hdmi_helper/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := hdmi_helper.cpp
 LOCAL_LDFLAGS := -L vendor/samsung/santos10-common/proprietary/vendor/lib
-LOCAL_LDLIBS := -lmultidisplay
+LOCAL_LDLIBS := libmultidisplay
 LOCAL_SHARED_LIBRARIES += libbinder liblog libutils
 LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
 LOCAL_MODULE := hdmi_helper
